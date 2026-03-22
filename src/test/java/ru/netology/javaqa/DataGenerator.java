2485713196436accb81cb56361dde75cfe27c37d
Denis.test <denis.galiulin.a@yandex.ru>
2026-03-22 17:45:59 +0500

    task 1

---
+++ src/test/java/ru/netology/javaqa/DataGenerator.java
@@ -0,0 +1,57 @@
+package ru.netology.javaqa;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        LocalDate date = LocalDate.now().plusDays(shift);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return date.format(formatter);
+    }
+
+    public static String generateCity(Faker faker) {
+        String[] cities = {"Москва", "Санкт-Петербург", "Казань", "Новосибирск",
+                "Екатеринбург", "Нижний Новгород", "Челябинск", "Самара"};
+        Random random = new Random();
+        return cities[random.nextInt(cities.length)];
+    }
+
+    public static String generateName(Faker faker) {
+        return faker.name().firstName() + " " + faker.name().lastName();
+    }
+
+    public static String generatePhone(Faker faker) {
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private static Faker faker;
+
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            faker = new Faker(new Locale(locale));
+            String city = generateCity(faker);
+            String name = generateName(faker);
+            String phone = generatePhone(faker);
+            return new UserInfo(city, name, phone);
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
---
+++ src/test/java/ru/netology/javaqa/DeliveryTest.java
@@ -0,0 +1,51 @@
+package ru.netology.javaqa;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+class DeliveryTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 3;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 6;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.LEFT_CONTROL, "a"), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Запланировать")).click();
+
+        $("[data-test-id='success-notification']")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate));
+
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.LEFT_CONTROL, "a"), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(secondMeetingDate);
+        $$("button").find(exactText("Запланировать")).click();
+
+        $("[data-test-id='replan-notification']")
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+
+        $$("button").find(exactText("Перепланировать")).click();
+
+        $("[data-test-id='success-notification']")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+}
