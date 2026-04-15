cca4b8f5b940e535428eaf7a01dc53f471f4979f
Denis.test <denis.galiulin.a@yandex.ru>
2026-04-15 14:06:51 +0500

    allure

--- src/test/java/ru/netology/javaqa/DeliveryTest.java
+++ src/test/java/ru/netology/javaqa/DeliveryTest.java
@@ -1,15 +1,26 @@
 package ru.netology.javaqa;
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
+
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 class DeliveryTest {
+
+    @BeforeAll
+    static void setupAll () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll () {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setup() {
